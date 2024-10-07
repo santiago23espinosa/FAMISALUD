b17cf8035f0b927582efce38018bdbb4157711e2
santiago23espinosa <santiago23espinosa@gmail.com>
2024-10-07 13:40:03 -0500

    inicio

---
+++ src/componentes/Header/Header.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './Header.css';
+
+const Header = () => {
+  return (
+    <header className="header">
+      <img src="/images/logo3.png" alt="FAMISALUD Logo" />
+      <nav>
+        <ul>
+          <li><a href="#inicio">Inicio</a></li>
+          <li><a href="#productos">Productos</a></li>
+          <li><a href="#servicios">Servicios</a></li>
+          <li><a href="#contacto">Contacto</a></li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
